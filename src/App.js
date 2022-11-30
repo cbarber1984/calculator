1f64f2b438ae56fb1dd47ad123f592a64bde4aa3
Craig <97862337+cbarber1984@users.noreply.github.com>
2022-11-30 05:29:48 -0500

    created components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is a Calculator</h1>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button Clicked!");
+          }}
+        />
+      </ButtonBox>
       <Footer />
-    </div>
+    </Wrapper>
   );
 }
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,31 +1,56 @@
 import React from "react";
 import { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
 
 class Footer extends Component {
   render() {
     return (
-      <div>
-        <p>
-          Calculator designed by{" "}
+      <div className="footer">
+        <div className="footer-line-one ">
+          <div className="footer-line-one-text ">
+            <p>
+              Calculator designed by Craig
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/cbarber1984"
+              >
+                <FontAwesomeIcon className="icon" icon={faGithub} />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/c_barber1984"
+              >
+                <FontAwesomeIcon className="icon" icon={faTwitter} />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/cbarber1984/"
+              >
+                <FontAwesomeIcon className="icon" icon={faLinkedinIn} />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="footer-line-two">
           <a
             target="_blank"
             rel="noreferrer"
-            href="https://github.com/cbarber1984"
+            href="https://icons8.com/icon/59744/calculator"
           >
-            Craig
+            Calculator
+          </a>{" "}
+          icon by{" "}
+          <a target="_blank" rel="noreferrer" href="https://icons8.com">
+            Icons8
           </a>
-        </p>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://icons8.com/icon/59744/calculator"
-        >
-          Calculator
-        </a>{" "}
-        icon by{" "}
-        <a target="_blank" rel="noreferrer" href="https://icons8.com">
-          Icons8
-        </a>
+        </div>
       </div>
     );
   }
